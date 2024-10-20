ae70e370c95cc75b6a097391c6f5ecd3a3e2d822
David610 <134853176+David610@users.noreply.github.com>
2024-10-20 22:44:59 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/TKN_0.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TKN_0.dir/main.c.obj"
+  "CMakeFiles/TKN_0.dir/main.c.obj.d"
+  "TKN_0.exe"
+  "TKN_0.exe.manifest"
+  "TKN_0.pdb"
+  "libTKN_0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TKN_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TKN_0.dir/main.c.obj.d
@@ -0,0 +1,13 @@
+CMakeFiles/TKN_0.dir/main.c.obj: \
+ C:\Users\Arina\CLionProjects\TKN_0\main.c \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/corecrt.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/ProgramData/mingw64/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h
